55007f873e8c7369ae66be0efa4a404f409a8a38
Benjamin Fischtein <49317408@est.ort.edu.ar>
2026-09-24 18:21:20 -0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
+import 'package:TP4_RIVERPOD/firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:TP4_RIVERPOD/core/routers.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
-  runApp(const ProviderScope(child: MyApp()));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
+
+  runApp(
+    const ProviderScope(
+      child: MyApp()
+    )
+  );
 }
 
 class MyApp extends StatelessWidget {
